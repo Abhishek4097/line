871b448adc66d84cc4c6c98046c78052655f2bd1
abhishek <abhishek.sharma_cs20@gla.ac.in>
2021-11-15 22:08:21 +0530

    Initial commit

---
+++ src/Command.java
@@ -0,0 +1,15 @@
+public class Command {
+    public static void main(String args[]) {
+        String s1 = args[0];
+        String s2 = args[1];
+        if ((s1.equals("GLA")) && (s2.equals("Mathura")))
+        {
+            System.out.println(s1+" University "+s2);
+        }
+        else if ((s1.equals("AAA")) && (s2.equals("Dehli")))
+        {
+            System.out.println(s1+" technologies "+s2);
+        }
+    }
+}
+
